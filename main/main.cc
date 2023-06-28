84a917510c013dddd41e17cc9e7ba11c07af6f57
huyenngn <huyenproducts@gmail.com>
2023-06-28 16:24:18 +0200

    merged codes

--- main/main.cc
+++ main/main.cc
@@ -9,10 +9,25 @@
 #include "freertos/semphr.h"
 #include "esp_system.h"
 #include "driver/gpio.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 /* Littlevgl specific */
 #include "lvgl/lvgl.h"
 #include "lvgl_helpers.h"
+
+/* CSI-Tool specific */
+#include "../_components/nvs_component.h"
+#include "../_components/sd_component.h"
+#include "../_components/csi_component.h"
+#include "../_components/input_component.h"
+#include "../_components/sockets_component.h"
 
 /*********************
  *      DEFINES
@@ -21,11 +36,50 @@
 #define LEFT_BUTTON_PIN GPIO_NUM_0
 #define RIGHT_BUTTON_PIN GPIO_NUM_35
 
+/*
+ * The examples use WiFi configuration that you can set via 'idf.py menuconfig'.
+ *
+ * If you'd rather not, just change the below entries to strings with
+ * the config you want - ie #define ESP_WIFI_SSID "mywifissid"
+ */
+#define ESP_WIFI_SSID CONFIG_ESP_WIFI_SSID
+#define ESP_WIFI_PASS CONFIG_ESP_WIFI_PASSWORD
+
+#ifdef CONFIG_WIFI_CHANNEL
+#define WIFI_CHANNEL CONFIG_WIFI_CHANNEL
+#else
+#define WIFI_CHANNEL 6
+#endif
+
+#ifdef CONFIG_SHOULD_COLLECT_CSI
+#define SHOULD_COLLECT_CSI 1
+#else
+#define SHOULD_COLLECT_CSI 0
+#endif
+
+#ifdef CONFIG_SHOULD_COLLECT_ONLY_LLTF
+#define SHOULD_COLLECT_ONLY_LLTF 1
+#else
+#define SHOULD_COLLECT_ONLY_LLTF 0
+#endif
+
+#ifdef CONFIG_SEND_CSI_TO_SERIAL
+#define SEND_CSI_TO_SERIAL 1
+#else
+#define SEND_CSI_TO_SERIAL 0
+#endif
+
+#ifdef CONFIG_SEND_CSI_TO_SD
+#define SEND_CSI_TO_SD 1
+#else
+#define SEND_CSI_TO_SD 0
+#endif
+
 /**********************
  *  STATIC PROTOTYPES
  **********************/
 static void lv_tick_task(void *arg);
-static void guiTask(void *pvParameter);
+// static void guiTask(void *pvParameter);
 static void show_menu(lv_obj_t *screen);
 static bool keyboard_read(lv_indev_drv_t *drv, lv_indev_data_t *data);
 
@@ -39,23 +93,12 @@ static lv_obj_t *tabview;
 static lv_group_t *g;
 static lv_obj_t *plot_label, *subc_label, *interval_label;
 
-/**********************
- *   APPLICATION MAIN
- **********************/
-void app_main()
-{
-    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
-     * Otherwise there can be problem such as memory corruption and so on.
-     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
-    xTaskCreatePinnedToCore(guiTask, "gui", 4096 * 2, NULL, 0, NULL, 1);
-}
-
 /* Creates a semaphore to handle concurrent call to lvgl stuff
  * If you wish to call *any* lvgl function from other threads/tasks
  * you should lock on the very same semaphore! */
 SemaphoreHandle_t xGuiSemaphore;
 
-static void guiTask(void *pvParameter)
+extern "C" void guiTask(void *pvParameter)
 {
     (void)pvParameter;
     xGuiSemaphore = xSemaphoreCreateMutex();
@@ -65,12 +108,12 @@ static void guiTask(void *pvParameter)
     /* Initialize SPI or I2C bus used by the drivers */
     lvgl_driver_init();
 
-    lv_color_t *buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    lv_color_t *buf1 = (lv_color_t *) heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf1 != NULL);
 
     /* Use double buffered when not working with monochrome displays */
 #ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    lv_color_t *buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    lv_color_t *buf2 = (lv_color_t *) heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf2 != NULL);
 #else
     static lv_color_t *buf2 = NULL;
@@ -165,34 +208,34 @@ static void guiTask(void *pvParameter)
     lv_coord_t z_array[9] = {170, 150, 130, 140, 170, 150, 130, 140, 150};
 
     lv_3d_chart_series_t *x1 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x1,(lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x1, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x2 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x2, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x2, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x3 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x3, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x3, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x4 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x4, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x4, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x5 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x5, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x5, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x6 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x6, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x6, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x7 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x7, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x7, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x8 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x8, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x8, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x9 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x9, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x9, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_3d_chart_series_t *x10 = lv_3d_chart_add_series(phase_chart);
-    lv_3d_chart_set_points(phase_chart, x10, (lv_coord_t *) &y_array, (lv_coord_t *) &z_array, 9);
+    lv_3d_chart_set_points(phase_chart, x10, (lv_coord_t *)&y_array, (lv_coord_t *)&z_array, 9);
 
     lv_obj_set_hidden(phase_chart, true);
 
@@ -200,13 +243,54 @@ static void guiTask(void *pvParameter)
         /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
         vTaskDelay(pdMS_TO_TICKS(10));
 
+        wifi_csi_info_t *data;
+
+        if (xQueueReceive(data_queue, &data, 100) == pdTRUE) {  // Daten aus queue holen, checke alle 0 ms falls voll
+
+            printf("\n______ PLOT ______\n");
+
+            int csi_len = data->len;  // Länge der CSI-Daten
+            int8_t *csi = data->buf;  // Pointer zeigt auf Buffer mit CSI Daten
+
+            // Extrahieren & Ausgabe der rohen CSI (Real- und Imaginärteil immer abwechselnd zwei Bytes)
+            std::stringstream csi_raw;
+            for (int i = 0; i < csi_len; i++) {
+                csi_raw << (int)csi[i] << " ";
+            }
+            csi_raw << "]\n";
+            printf("CSI_RAW = %s \n", csi_raw.str().c_str());
+
+            /*
+            // Extrahieren, Berechnen und Ausgabe der der CSI-Amplituden
+            std::stringstream csi_amp;
+            for (int i = 0; i < csi_len / 2; i++) {
+                csi_amp << (int) sqrt(pow(csi[i * 2], 2) + pow(csi[(i * 2) + 1], 2)) << " ";
+            }
+            csi_amp << "]\n";
+            printf("CSI_AMP = %s \n", csi_amp.str().c_str());
+
+            // Extrahieren, Berechnen und Ausgabe der CSI-Phasen
+            std::stringstream csi_phase;
+            for (int i = 0; i < csi_len / 2; i++) {
+                csi_phase << (int) atan2(csi[i*2], csi[(i*2)+1]) << " ";
+            }
+            csi_phase << "]\n";
+            printf("CSI_AMP = %s \n", csi_amp.str().c_str());
+            */
+            fflush(stdout);
+            vTaskDelay(0);
+        }
+        else {
+            int space = uxQueueSpacesAvailable(data_queue);
+            printf("Freier Platz in Queue: %d \n", space);
+        }
+
         /* Try to take the semaphore, call lvgl related function on success */
         if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
             lv_task_handler();
             xSemaphoreGive(xGuiSemaphore);
         }
     }
-
     /* A task should NEVER return */
     free(buf1);
 #ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
@@ -214,6 +298,7 @@ static void guiTask(void *pvParameter)
 #endif
     vTaskDelete(NULL);
 }
+
 
 static void lv_tick_task(void *arg)
 {
@@ -364,4 +449,161 @@ static void show_menu(lv_obj_t *screen)
 
     lv_obj_t *interval_info = lv_label_create(tab3, plot_info);
     lv_label_set_text(interval_info, "Configure update interval");
+}
+
+///////////////////////     ENDE MEIN CODE     ///////////////////////////
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about one event
+ * - are we connected to the AP with an IP? */
+const int WIFI_CONNECTED_BIT = BIT0;
+
+static const char *TAG = "Active CSI collection (Station)";
+
+esp_err_t _http_event_handle(esp_http_client_event_t *evt)
+{
+    switch (evt->event_id) {
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                if (!real_time_set) {
+                    char *data = (char *)malloc(evt->data_len + 1);
+                    strncpy(data, (char *)evt->data, evt->data_len);
+                    data[evt->data_len + 1] = '\0';
+                    time_set(data);
+                    free(data);
+                }
+            }
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
+//// en_sys_seq: see https://github.com/espressif/esp-idf/blob/master/docs/api-guides/wifi.rst#wi-fi-80211-packet-send for details
+esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGI(TAG, "Retry connecting to the AP");
+        esp_wifi_connect();
+        xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "Got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+bool is_wifi_connected()
+{
+    return (xEventGroupGetBits(s_wifi_event_group) & WIFI_CONNECTED_BIT);
+}
+
+void station_init()
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_sta_config_t wifi_sta_config = {};
+    wifi_sta_config.channel = WIFI_CHANNEL;
+    wifi_config_t wifi_config = {
+        .sta = wifi_sta_config,
+    };
+
+    strlcpy((char *)wifi_config.sta.ssid, ESP_WIFI_SSID, sizeof(ESP_WIFI_SSID));
+    strlcpy((char *)wifi_config.sta.password, ESP_WIFI_PASS, sizeof(ESP_WIFI_PASS));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    esp_wifi_set_ps(WIFI_PS_NONE);
+
+    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
+}
+
+TaskHandle_t xHandle = NULL;
+
+void vTask_socket_transmitter_sta_loop(void *pvParameters)
+{
+    for (;;) {
+        socket_transmitter_sta_loop(&is_wifi_connected);
+    }
+}
+
+void config_print()
+{
+    printf("\n\n\n\n\n\n\n\n");
+    printf("-----------------------\n");
+    printf("ESP32 CSI Tool Settings\n");
+    printf("-----------------------\n");
+    printf("PROJECT_NAME: %s\n", "ACTIVE_STA");
+    printf("CONFIG_ESPTOOLPY_MONITOR_BAUD: %d\n", CONFIG_ESPTOOLPY_MONITOR_BAUD);
+    printf("CONFIG_ESP_CONSOLE_UART_BAUDRATE: %d\n", CONFIG_ESP_CONSOLE_UART_BAUDRATE);
+    printf("IDF_VER: %s\n", IDF_VER);
+    printf("-----------------------\n");
+    printf("WIFI_CHANNEL: %d\n", WIFI_CHANNEL);
+    printf("ESP_WIFI_SSID: %s\n", ESP_WIFI_SSID);
+    printf("ESP_WIFI_PASSWORD: %s\n", ESP_WIFI_PASS);
+    printf("PACKET_RATE: %i\n", CONFIG_PACKET_RATE);
+    printf("SHOULD_COLLECT_CSI: %d\n", SHOULD_COLLECT_CSI);
+    printf("SHOULD_COLLECT_ONLY_LLTF: %d\n", SHOULD_COLLECT_ONLY_LLTF);
+    printf("SEND_CSI_TO_SERIAL: %d\n", SEND_CSI_TO_SERIAL);
+    printf("SEND_CSI_TO_SD: %d\n", SEND_CSI_TO_SD);
+    printf("-----------------------\n");
+    printf("\n\n\n\n\n\n\n\n");
+}
+
+extern "C" void app_main()
+{
+    config_print();
+    nvs_init();
+    sd_init();
+    station_init();
+    csi_init((char *)"STA");
+
+#if !(SHOULD_COLLECT_CSI)
+    printf("CSI will not be collected. Check `idf.py menuconfig  # > ESP32 CSI Tool Config` to enable CSI");
+#endif
+
+    // xTaskCreatePinnedToCore(&vTask_socket_transmitter_sta_loop, "socket_transmitter_sta_loop",
+    //                         10000, (void *)&is_wifi_connected, 100, &xHandle, 0);
+
+    /////////////// HIER TASK ERSTELLEN ///////////////
+
+    // für task visualize_data sind erstmal 1 kByte reserviert und Priorität liegt bei zwei
+
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096 * 2, NULL, 0, NULL, 1);
+
+    ///////////////////////////////////////////////////
 }
